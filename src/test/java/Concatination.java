1802515316d6db3d3f83619720b14b2a7da95a7b
mubeen7786 <107865479+mubeen7786@users.noreply.github.com>
2022-06-21 04:03:46 +0500

    Concatination program

---
+++ src/test/java/Concatination.java
@@ -0,0 +1,9 @@
+public class Concatination {
+        public static void main(String args[]){
+            String s1="Mubeen ";
+            String s2="Akhtar";
+            String s3=s1.concat(s2);
+            System.out.println(s3); //Mubeen Akhtar
+        }
+    }
+
---
+++ src/test/java/firstFile.java
@@ -0,0 +1,24 @@
+public class firstFile {
+    public static void main(String[] args)
+//    {
+//        String s="Hello ";
+//        String m = " This";
+//        String x = " Is ";
+//        String x1 = " Mubeen ";
+//        String myname = s + m + x + x1;
+//        System.out.println(myname);
+//    }
+
+// Another method of concatination is
+{
+    String s1="Mubeen ";
+    String s2="Akhtar";
+    String s5="Love";
+    String s3=s1.concat(s2);
+    System.out.println("value fo the S1 is"+ s1 + "And Value of the S2 is " + s2 + "When we concatinate these two stigns value will = " + s3 +" concatination"); //Mubeen Akhtar
+    String s4 = s3.concat(s5);
+    System.out.println("This is my second concatination = " + s4);
+}
+
+
+}
